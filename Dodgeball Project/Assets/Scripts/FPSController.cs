d8c8fe2656485963a056eb979f9a2c8841859044
taio1000 <tadeoelli@hotmail.com>
2024-02-11 23:33:40 -0300

    changes

--- Dodgeball Project/Assets/Scripts/FPSController.cs
+++ Dodgeball Project/Assets/Scripts/FPSController.cs
@@ -149,7 +149,7 @@ public class FPSController : MonoBehaviour
 
     public void ShieldPowerUp(){
         shield.SetActive(true);
-        Invoke("ShieldPowerUpEnd", 5.0f);
+        Invoke("ShieldPowerUpEnd", 7.0f);
     }
 
     private void ShieldPowerUpEnd(){
