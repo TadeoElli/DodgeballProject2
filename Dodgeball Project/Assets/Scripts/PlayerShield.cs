53e8e850f09f7783166e7aefabe2976217c6fcb6
taio1000 <tadeoelli@hotmail.com>
2024-02-12 21:11:07 -0300

    Update Camera Shield Shader

--- Dodgeball Project/Assets/Scripts/PlayerShield.cs
+++ Dodgeball Project/Assets/Scripts/PlayerShield.cs
@@ -9,26 +9,41 @@ public class PlayerShield : MonoBehaviour
     // Start is called before the first frame update
     [Header("Shield")]
     public GameObject shield;
+    private bool isEnding = false;
+    private float timer = 0f;
     [SerializeField] private ScriptableRendererFeature _fullScreenShield;
+    [SerializeField] private Material fullScreenShield_Material;
+
     void Start()
     {
         _fullScreenShield.SetActive(false);
+        fullScreenShield_Material.SetFloat("_LerpEndings", 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isEnding){
+            timer = timer + 0.5f * Time.deltaTime;
+            fullScreenShield_Material.SetFloat("_LerpEndings", timer);
+        }
     }
 
     public void ShieldPowerUp(){
         shield.SetActive(true);
         _fullScreenShield.SetActive(true);
         Invoke("ShieldPowerUpEnd", 10f);
+        Invoke("StartEnding", 8f);
     }
 
+    private void StartEnding(){
+        isEnding = true;
+    }
     private void ShieldPowerUpEnd(){
         shield.SetActive(false);
+        fullScreenShield_Material.SetFloat("_LerpEndings", 0f);
+        timer  = 0f;
+        isEnding = false;
         _fullScreenShield.SetActive(false);
     }
 }
